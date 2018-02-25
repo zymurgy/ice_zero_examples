76ed9a0fbb52c8786d1fb57fedb7c6962c506fce
Erik Moe <e.moe@rcn.com>
2018-02-25 00:23:46 -0600

    More SPI slave cleanup

--- spi_slave/spi_slave_top.v
+++ spi_slave/spi_slave_top.v
@@ -1,13 +1,13 @@
 // Top module
 
 module spi_slave_top (
-  input clk_100mhz,
-  input pi_spi_sck,
-  input pi_spi_mosi,
+  input  clk_100mhz,
+  input  pi_spi_sck,
+  input  pi_spi_mosi,
   output pi_spi_miso,
-  input pi_spi_ce0,
+  input  pi_spi_ce0,
   output ok_led
-);
+  );
 
   spi_slave slave (
     .clk (clk_100mhz),
